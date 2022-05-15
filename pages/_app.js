f3cf88ab8e0e091e8d7d94ee4b81e60fe90f8f1b
Freddylo <freddy_luo@moneynet.tw>
2022-05-15 16:23:01 +0800

    chore: prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.scss'
+/* eslint-disable react/prop-types */
+import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
